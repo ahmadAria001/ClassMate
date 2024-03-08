54a7961956347f941231fe88110d13c89b9059dc
Ahmad Aria <73412887+ahmadAria001@users.noreply.github.com>
2024-03-08 07:27:59 +0700

    initial commit (#1)

---
+++ resources/js/Pages/Home.svelte
@@ -0,0 +1,9 @@
+<script>
+    let text = "Hello World!";
+</script>
+
+<div class="w-full h-screen flex justify-center align-middle">
+    <h1 class="text-3xl text-sky-600 m-auto">
+        {text}
+    </h1>
+</div>
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ["resources/css/app.postcss", "resources/js/app.js"],
+            refresh: true,
+        }),
+        svelte({}),
+    ],
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.svelte", { eager: true });
+        return pages[`./Pages/${name}.svelte`];
+    },
+});
