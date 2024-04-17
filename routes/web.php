e08dce1b8a0a43b4caee939b7aa4c151e339f00c
ahmadAria001 <razscer0@gmail.com>
2024-04-17 08:30:01 +0700

    fix: minor

--- routes/web.php
+++ routes/web.php
@@ -33,97 +33,101 @@ Route::get('/dues', function () {
     return Inertia::render('Auth/Dues');
 })->name('dues');
 
-Route::prefix('/auth')->group(fn () => [
-    Route::post('/signin', Login::class)
-]);
+Route::prefix('/auth')->group(fn() => [Route::post('/signin', Login::class)]);
 
+// Route::get('/family', FamilyController::class);
+// Route::prefix('/family')->group(function () {
+//     Route::get('/', FamilyController::class);
+//     Route::get('/create', [FamilyController::class, 'createView']);
+//     Route::post('/store', [FamilyController::class, 'store']);
+//     Route::get('/detail/{family_id}', [FamilyController::class, 'detail']);
+//     Route::get('/edit/{family_id}', [FamilyController::class, 'editView']);
+//     Route::post('/update/{family_id}', [FamilyController::class, 'update']);
+//     Route::get('/delete/{family_id}', [FamilyController::class, 'delete']);
+// });
+//
+// Route::get('/bansos', FinancialAssistanceController::class);
+// Route::prefix('/bansos')->group(function () {
+//     Route::get('/', FinancialAssistanceController::class);
+//     Route::get('/create', [FinancialAssistanceController::class, 'createView']);
+//     Route::post('/store', [FinancialAssistanceController::class, 'store']);
+//     Route::get('/detail/{bansos_id}', [FinancialAssistanceController::class, 'detail']);
+//     Route::get('/edit/{bansos_id}', [FinancialAssistanceController::class, 'editView']);
+//     Route::post('/update/{bansos_id}', [FinancialAssistanceController::class, 'update']);
+//     Route::get('/delete/{bansos_id}', [FinancialAssistanceController::class, 'delete']);
+// });
+//
+// // 404
+// Route::get('/404', function () {
+//     return Inertia::render('Page404');
+// })->name('404');
+//
+// // profile
+// Route::get('/profile', function () {
+//     return Inertia::render('Profile');
+// })->name('Profile');
+//
+// // warga
+// Route::get('/status-pengaduan', function () {
+//     return Inertia::render('StatusPengaduan');
+// })->name('StatusPengaduan');
+// Route::get('/status-pengajuan', function () {
+//     return Inertia::render('StatusPengajuan');
+// })->name('Statuspengajuan');
+// Route::get('/status-bansos', function () {
+//     return Inertia::render('StatusBansos');
+// })->name('StatusBansos');
+//
+// // RT dan RW
+// Route::get('/warga-rt', function () {
+//     return Inertia::render('PendudukByRT');
+// })->name('PendudukByRT');
+// Route::get('/arsip-penduduk', function () {
+//     return Inertia::render('ArsipPenduduk');
+// })->name('ArsipPenduduk');
+// Route::get('/daftar-pengaduan', function () {
+//     return Inertia::render('DaftarPengaduan');
+// })->name('DaftarPengaduan');
+// Route::get('/daftar-pengajuan-surat', function () {
+//     return Inertia::render('DaftarPermintaanSurat');
+// })->name('DaftarPermintaanSurat');
+// Route::get('/keuangan', function () {
+//     return Inertia::render('KeuanganRT');
+// })->name('KeuanganRT');
+// Route::get('/daftar-bansos', function () {
+//     return Inertia::render('DaftarBansos');
+// })->name('DaftarBansos');
+// Route::get('/pengumuman', function () {
+//     return Inertia::render('Pengumuman');
+// })->name('Pengumuman');
+// Route::get('/kegiatan-warga', function () {
+//     return Inertia::render('KegiatanWarga');
+// })->name('KegiatanWarga');
+//
+// Route::get('/daftar-rt', function () {
+//     return Inertia::render('DaftarRT');
+// })->name('DaftarRT');
 
-Route::get('/family', FamilyController::class);
-Route::prefix('/family')->group(function () {
-    Route::get('/', FamilyController::class);
-    Route::get('/create', [FamilyController::class, 'createView']);
-    Route::post('/store', [FamilyController::class, 'store']);
-    Route::get('/detail/{family_id}', [FamilyController::class, 'detail']);
-    Route::get('/edit/{family_id}', [FamilyController::class, 'editView']);
-    Route::post('/update/{family_id}', [FamilyController::class, 'update']);
-    Route::get('/delete/{family_id}', [FamilyController::class, 'delete']);
-});
-
-Route::get('/bansos', FinancialAssistanceController::class);
-Route::prefix('/bansos')->group(function () {
-    Route::get('/', FinancialAssistanceController::class);
-    Route::get('/create', [FinancialAssistanceController::class, 'createView']);
-    Route::post('/store', [FinancialAssistanceController::class, 'store']);
-    Route::get('/detail/{bansos_id}', [FinancialAssistanceController::class, 'detail']);
-    Route::get('/edit/{bansos_id}', [FinancialAssistanceController::class, 'editView']);
-    Route::post('/update/{bansos_id}', [FinancialAssistanceController::class, 'update']);
-    Route::get('/delete/{bansos_id}', [FinancialAssistanceController::class, 'delete']);
-});
-
-// 404
-Route::get('/404', function () {
-    return Inertia::render('Page404');
-})->name('404');
-
-// profile
-Route::get('/profile', function () {
-    return Inertia::render('Profile');
-})->name('Profile');
-
-// warga
-Route::get('/status-pengaduan', function () {
-    return Inertia::render('StatusPengaduan');
-})->name('StatusPengaduan');
-Route::get('/status-pengajuan', function () {
-    return Inertia::render('StatusPengajuan');
-})->name('Statuspengajuan');
-Route::get('/status-bansos', function () {
-    return Inertia::render('StatusBansos');
-})->name('StatusBansos');
-
-// RT dan RW
-Route::get('/warga-rt', function () {
-    return Inertia::render('PendudukByRT');
-})->name('PendudukByRT');
-Route::get('/arsip-penduduk', function () {
-    return Inertia::render('ArsipPenduduk');
-})->name('ArsipPenduduk');
-Route::get('/daftar-pengaduan', function () {
-    return Inertia::render('DaftarPengaduan');
-})->name('DaftarPengaduan');
-Route::get('/daftar-pengajuan-surat', function () {
-    return Inertia::render('DaftarPermintaanSurat');
-})->name('DaftarPermintaanSurat');
-Route::get('/keuangan', function () {
-    return Inertia::render('KeuanganRT');
-})->name('KeuanganRT');
-Route::get('/daftar-bansos', function () {
-    return Inertia::render('DaftarBansos');
-})->name('DaftarBansos');
-Route::get('/pengumuman', function () {
-    return Inertia::render('Pengumuman');
-})->name('Pengumuman');
-Route::get('/kegiatan-warga', function () {
-    return Inertia::render('KegiatanWarga');
-})->name('KegiatanWarga');
-
-Route::get('/daftar-rt', function () {
-    return Inertia::render('DaftarRT');
-})->name('DaftarRT');
-
-function loadRoutes($dir)
+function loadRoutesWEB($dir)
 {
-    if (!is_dir($dir)) return;
-
+    if (!is_dir($dir)) {
+        return;
+    }
+    error_log($dir);
     $files = scandir($dir);
     foreach ($files as $file) {
-        if ($file == '.' || $file == '..') continue;
+        if ($file == '.' || $file == '..') {
+            continue;
+        }
 
         $filePath = $dir . '/' . $file;
-        if (is_file($filePath) && pathinfo($filePath)['extension'] === 'php')
+        error_log($filePath);
+        if (is_file($filePath) && pathinfo($filePath)['extension'] === 'php') {
             require_once $filePath;
-        else if (is_dir($filePath)) loadRoutes($filePath);
+        } elseif (is_dir($filePath)) {
+            loadRoutes($filePath);
+        }
     }
 }
 
-loadRoutes(__DIR__ . '/Handler/WEB');
+loadRoutesWEB(__DIR__ . '/Handler/WEB');
