a9ac705b3e14c3384ba55a5d9b1c871f1c83a087
ahmadAria001 <razscer0@gmail.com>
2024-05-15 07:13:47 +0700

    feat: user profile picture routes

--- routes/Handler/API/user.php
+++ routes/Handler/API/user.php
@@ -1,18 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileImageController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\Resources\User\Create;
 use App\Http\Middleware\Resources\User\Delete;
 use App\Http\Middleware\Resources\User\GET;
+use App\Http\Middleware\Resources\User\Pict\Create as PictCreate;
+use App\Http\Middleware\Resources\User\Pict\Get as PictGet;
 use App\Http\Middleware\Resources\User\Update;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/user')->group(
     fn() => [
-        Route::get('/{column}/{operator}/{value}', [UserController::class, 'getCustom'])->middleware(GET::class),
+        Route::prefix('/img')->group(fn()=>[
+            Route::get('/{filter}',[ProfileImageController::class,'get'])->middleware(PictGet::class),
+            Route::post('/',[ProfileImageController::class,'create'])->middleware(PictCreate::class),
+        ]),
 
-        // Route::get('/cvl', [UserController::class, 'withCivils'])->middleware(GetWCV::class),
-        // Route::get('/cvl/{filter}', [UserController::class, 'withCivils'])->middleware(GetWCV::class),
+        Route::get('/{column}/{operator}/{value}', [UserController::class, 'getCustom'])->middleware(GET::class),
 
         Route::get('/', [UserController::class, 'get'])->middleware(GET::class),
         Route::get('/{filter}', [UserController::class, 'get'])->middleware(GET::class),
