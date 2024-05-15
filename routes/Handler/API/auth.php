da74c969ea2a1be194b3c00efa41c9bf76475d98
ahmadAria001 <razscer0@gmail.com>
2024-05-15 07:12:57 +0700

    feat: password reset

--- routes/Handler/API/auth.php
+++ routes/Handler/API/auth.php
@@ -2,10 +2,13 @@
 
 use App\Http\Controllers\Auth\Login;
 use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\PasswordController;
+use App\Http\Middleware\Resources\Password\Reset;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/auth')->group(fn () => [
     Route::post('/signin', Login::class),
-    Route::get('/signout',Logout::class)
+    Route::get('/signout', Logout::class),
+    Route::post('/pw/reset', [PasswordController::class,'reset'])->middleware(Reset::class),
 ]);
