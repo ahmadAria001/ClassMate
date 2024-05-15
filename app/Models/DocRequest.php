87269c38e9109b5d01a053b986ee4cc26ff70968
rickytedjo <rickyputra.tedjo@gmail.com>
2024-05-15 12:42:43 +0700

    Doc Request Model

---
+++ app/Models/DocRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DocRequest extends Model
+{
+    use HasFactory, SoftDeletes;
+    
+    protected $table = 'doc_request';
+    protected $fillable = [
+        'requestStatus',
+        'request_by',
+        'docs_id',
+        'created_at',
+        'created_by',
+        'updated_at',
+        'updated_by',
+        'deleted_at',
+        'deleted_by'
+    ];
+
+    protected $dateFormat = 'U';
+
+    public function request_by(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'request_by', 'id');
+    }
+
+    public function created_by(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by', 'id');
+    }
+
+    public function updated_by(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'updated_by', 'id');
+    }
+
+    public function deleted_by(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'deleted_by', 'id');
+    }
+}
