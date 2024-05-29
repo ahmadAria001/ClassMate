bcaa2800fa095d35dbd1cd38642b13266a6e2bb9
ahmadAria001 <razscer0@gmail.com>
2024-05-29 13:15:05 +0700

    fix: minor

--- app/Http/Controllers/ProfileImageController.php
+++ app/Http/Controllers/ProfileImageController.php
@@ -8,8 +8,9 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Laravel\Facades\Image;
 use Laravel\Sanctum\PersonalAccessToken;
+use Spatie\Image\Image;
+use Spatie\Image\Manipulations;
 
 class ProfileImageController extends Controller
 {
@@ -52,27 +53,41 @@ class ProfileImageController extends Controller
 
         $model = User::withoutTrashed()->where('id', $user)->first();
 
-        $name = Carbon::now() . '_' . $image->getClientOriginalName();
+        $name = Carbon::now() . '_' . $image->hashName();
 
-        if (!Storage::directoryExists('assets' . DIRECTORY_SEPARATOR . 'uploads')) {
-            // File::makeDirectory(, 0755, true, true);
-            Storage::makeDirectory('assets' . DIRECTORY_SEPARATOR . 'uploads');
+        try {
+            if (!Storage::directoryExists('public' . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . 'uploads')) {
+                // File::makeDirectory(, 0755, true, true);
+                Storage::makeDirectory('public' . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . 'uploads');
+            }
+
+            $path =
+                // public_path('storage') . DIRECTORY_SEPARATOR .
+                'public' . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR;
+
+            $loadedImg = Image::load($image->getRealPath());
+            $compressedImg = $loadedImg
+                ->width(480)
+                ->height(480)
+                ->quality(20)
+                ->save(Storage::path($path) . $name);
+
+            // Image::read($image)->resizeDown(480, 480)->toJpeg();
+            // Storage::putFileAs($path, $compressedImg, $name);
+            // [$width, $height] = getimagesize($image->getFileInfo());
+
+            error_log($name);
+            error_log($compressedImg);
+
+            $model->pict = $name;
+            $model->save();
+
+            return Response()->json(['status' => true, 'message' => 'Operation Success'], 200);
+        } catch (\Throwable $th) {
+            error_log('Line ' . $th->getLine() . ' ' . $th->getFile());
+            error_log($th->getMessage());
+            return Response()->json(['status' => false, 'message' => 'Something went wrong'], 500);
         }
-
-        $path =
-            // public_path('storage') . DIRECTORY_SEPARATOR .
-            'assets' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR;
-        Storage::putFileAs($path, $image, $name);
-        // [$width, $height] = getimagesize($image->getFileInfo());
-        error_log($path);
-        // error_log(asset('storage/.gitignore'));
-
-        Image::read($image)->resize(480, 480)->toJpeg()->save($path . $name);
-
-        $model->pict = $name;
-        $model->save();
-
-        return Response()->json(['status' => true, 'message' => 'Operation Success'], 200);
     }
 
     public function edit()
