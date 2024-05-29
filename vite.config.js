fd5f5a177db7a2944755cc29e21f1f602905e5e6
ahmadAria001 <razscer0@gmail.com>
2024-05-29 15:46:21 +0700

    fix: minor

--- vite.config.js
+++ vite.config.js
@@ -18,14 +18,18 @@ export default defineConfig({
             tsconfig: "./tsconfig.json",
         }),
     ],
-    resolve: {
-        alias: {
-            "@P": path.resolve(__dirname, "resources/js/Pages"),
-            "@R": path.resolve(__dirname, "resources/js/"),
-            "@C": path.resolve(__dirname, "resources/js/Components"),
-            "@U": path.resolve(__dirname, "resources/js/Utils)"),
-        },
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.svelte", { eager: true });
+        return pages[`./Pages/${name}.svelte`];
     },
+    //  {
+    //     alias: {
+    //         "@P": path.resolve(__dirname, "resources/js/Pages"),
+    //         "@R": path.resolve(__dirname, "resources/js/"),
+    //         "@C": path.resolve(__dirname, "resources/js/Components"),
+    //         "@U": path.resolve(__dirname, "resources/js/Utils)"),
+    //     },
+    // },
     //  (name) => {
     //     const pages = import.meta.glob("./Pages/**/*.svelte", { eager: true });
     //     return pages[`./Pages/${name}.svelte`];
